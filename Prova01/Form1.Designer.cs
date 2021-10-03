5cc94f69f5b978e7c9979787c12beb5bdc834357
LucasGuimara3s <91847150+LucasGuimara3s@users.noreply.github.com>
2021-10-03 16:40:50 -0300

    Add files via upload

---
+++ Prova01/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Prova01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.rbSoma = new System.Windows.Forms.RadioButton();
+            this.rbSub = new System.Windows.Forms.RadioButton();
+            this.rbMult = new System.Windows.Forms.RadioButton();
+            this.rbDiv = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resultado";
+            // 
+            // txtN1
+            // 
+            this.txtN1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN1.Location = new System.Drawing.Point(95, 10);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(220, 26);
+            this.txtN1.TabIndex = 3;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN2.Location = new System.Drawing.Point(94, 50);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(221, 26);
+            this.txtN2.TabIndex = 4;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(96, 187);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(219, 26);
+            this.txtResultado.TabIndex = 5;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(230, 95);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(85, 67);
+            this.btnLimpar.TabIndex = 6;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // rbSoma
+            // 
+            this.rbSoma.AutoSize = true;
+            this.rbSoma.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbSoma.Location = new System.Drawing.Point(16, 95);
+            this.rbSoma.Name = "rbSoma";
+            this.rbSoma.Size = new System.Drawing.Size(68, 22);
+            this.rbSoma.TabIndex = 7;
+            this.rbSoma.TabStop = true;
+            this.rbSoma.Text = "Soma";
+            this.rbSoma.UseVisualStyleBackColor = true;
+            this.rbSoma.Click += new System.EventHandler(this.rbSoma_Click);
+            // 
+            // rbSub
+            // 
+            this.rbSub.AutoSize = true;
+            this.rbSub.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbSub.Location = new System.Drawing.Point(12, 140);
+            this.rbSub.Name = "rbSub";
+            this.rbSub.Size = new System.Drawing.Size(98, 22);
+            this.rbSub.TabIndex = 8;
+            this.rbSub.TabStop = true;
+            this.rbSub.Text = "Subtração";
+            this.rbSub.UseVisualStyleBackColor = true;
+            this.rbSub.Click += new System.EventHandler(this.rbSub_Click);
+            // 
+            // rbMult
+            // 
+            this.rbMult.AutoSize = true;
+            this.rbMult.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMult.Location = new System.Drawing.Point(107, 96);
+            this.rbMult.Name = "rbMult";
+            this.rbMult.Size = new System.Drawing.Size(117, 22);
+            this.rbMult.TabIndex = 9;
+            this.rbMult.TabStop = true;
+            this.rbMult.Text = "Multiplicação";
+            this.rbMult.UseVisualStyleBackColor = true;
+            this.rbMult.Click += new System.EventHandler(this.rbMult_Click);
+            // 
+            // rbDiv
+            // 
+            this.rbDiv.AutoSize = true;
+            this.rbDiv.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDiv.Location = new System.Drawing.Point(107, 140);
+            this.rbDiv.Name = "rbDiv";
+            this.rbDiv.Size = new System.Drawing.Size(79, 22);
+            this.rbDiv.TabIndex = 10;
+            this.rbDiv.TabStop = true;
+            this.rbDiv.Text = "Divisão";
+            this.rbDiv.UseVisualStyleBackColor = true;
+            this.rbDiv.Click += new System.EventHandler(this.rbDiv_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(327, 228);
+            this.Controls.Add(this.rbDiv);
+            this.Controls.Add(this.rbMult);
+            this.Controls.Add(this.rbSub);
+            this.Controls.Add(this.rbSoma);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.RadioButton rbSoma;
+        private System.Windows.Forms.RadioButton rbSub;
+        private System.Windows.Forms.RadioButton rbMult;
+        private System.Windows.Forms.RadioButton rbDiv;
+    }
+}
+
---
+++ Prova01/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prova01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtN1.Text = "";
+            txtN2.Text = "";
+            txtResultado.Text = "";
+        }
+
+        private void rbSoma_Click(object sender, EventArgs e)
+        {        
+            if (txtN1.Text == "" || txtN2.Text == "")
+            {
+                MessageBox.Show("Por favor, digite os números para serem calculados!");
+                rbSoma.Checked = false;
+                return;
+            }
+
+            double n1 = 0, n2 = 0;
+
+            if (rbSoma.Checked == true && txtResultado.Text != "" && double.Parse(txtResultado.Text) == double.Parse(txtN1.Text) + double.Parse(txtN2.Text))
+            {
+                txtN1.Text = txtResultado.Text;
+            }
+
+            rbSoma.Checked = true;
+
+            n1 = Convert.ToDouble(txtN1.Text.Trim());
+            n2 = Convert.ToDouble(txtN2.Text.Trim());
+
+            txtResultado.Text = "";
+            txtResultado.Text = (n1 + n2).ToString();
+        }
+
+        private void rbSub_Click(object sender, EventArgs e)
+        {
+            double n1 = 0, n2 = 0;
+
+            if (txtN1.Text == "" || txtN2.Text == "")
+            {
+                MessageBox.Show("Por favor, digite os números para serem calculados!");
+                rbSub.Checked = false;
+                return;
+            }
+
+            if (rbSub.Checked == true && txtResultado.Text != "" && double.Parse(txtResultado.Text) == double.Parse(txtN1.Text) - double.Parse(txtN2.Text))
+            {
+                txtN1.Text = txtResultado.Text;
+            }
+
+            rbSub.Checked = true;
+
+            n1 = Convert.ToDouble(txtN1.Text.Trim());
+            n2 = Convert.ToDouble(txtN2.Text.Trim());
+
+            txtResultado.Text = "";
+            txtResultado.Text = (n1 - n2).ToString();
+        }
+
+        private void rbMult_Click(object sender, EventArgs e)
+        {
+            double n1 = 0, n2 = 0;
+
+            if (txtN1.Text == "" || txtN2.Text == "")
+            {
+                MessageBox.Show("Por favor, digite os números para serem calculados!");
+                rbMult.Checked = false;
+                return;
+            }
+
+            if (rbMult.Checked == true && txtResultado.Text != "" && double.Parse(txtResultado.Text) == double.Parse(txtN1.Text) * double.Parse(txtN2.Text))
+                txtN1.Text = txtResultado.Text;
+
+            rbMult.Checked = true;
+
+            n1 = Convert.ToDouble(txtN1.Text.Trim());
+            n2 = Convert.ToDouble(txtN2.Text.Trim());
+
+            txtResultado.Text = "";
+            txtResultado.Text = (n1 * n2).ToString();
+        }
+
+        private void rbDiv_Click(object sender, EventArgs e)
+        {
+
+            double n1 = 0, n2 = 0;
+
+            if (txtN1.Text == "" || txtN2.Text == "")
+            {
+                MessageBox.Show("Por favor, digite os números para serem calculados!");
+                rbDiv.Checked = false;
+                return;
+            }
+            if (txtN2.Text.Trim() == "0" || Convert.ToInt32(txtN2.Text.Trim()) == 0)
+            {
+                MessageBox.Show("Não é possível dividir um número por 0.\nPor favor digite outro número.");
+                rbDiv.Checked = false;
+                return;
+            }
+
+            if (rbDiv.Checked == true && txtResultado.Text != "" && double.Parse(txtResultado.Text) == double.Parse(txtN1.Text) / double.Parse(txtN2.Text))
+            {
+                txtN1.Text = txtResultado.Text;
+            }
+
+            n1 = Convert.ToDouble(txtN1.Text.Trim());
+            n2 = Convert.ToDouble(txtN2.Text.Trim());
+
+            rbDiv.Checked = true;
+
+            txtResultado.Text = "";
+            txtResultado.Text = (n1 / n2).ToString();
+        }
+    }
+}
